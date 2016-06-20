1082f01e19398923251d90b2e60d57516801fda1
Alex Towli <alextowli@gmail.com>
2016-06-20 22:12:17 +0900

    remove turbolinks gem to easily use jquery

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,6 +1,12 @@
-$(document).ready(function() {
-	$(".reply-link").click(function() {
-		var id = $(this).attr('id');
-		$("#"+id+".post-reply-form").toggle();
+var ready = function(){
+	var form_class_name = 'post-reply-form';
+	var reply_link_class_name = 'reply-link';
+
+	$('.reply-link').on('click', function() {
+		var id = this.id;
+		console.log(id);
+		$('#'+id+'.post-reply-form').toggle();
 	});
-});
+};
+
+$(document).ready(ready);
